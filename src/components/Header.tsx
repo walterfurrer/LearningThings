49ea1157c18ef76798b126ba7a11623c4fcc27a5
Walter Furrer <walterfurrer@proton.me>
2025-06-30 16:19:01 -0400

    Update header styling with gradient backgrounds

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,13 @@ const Header = () => {
   return (
     <header className="flex flex-col gap-8 mt-32">
       <div>
-        <h1 className="text-5xl font-bold flex flex-col gap-2 lg:flex-row flex-wrap">
-          Learning <span className="text-zinc-950 bg-js-yellow px-4 py-2">JavaScript</span>
-          <span className="bg-ts-blue px-4 py-2">TypeScript</span>{" "}
-          <span className="text-zinc-950 bg-react-blue px-4 py-2">React</span>
+        <h1 className="text-5xl font-bold flex flex-col gap-2 px-4 py-2 lg:flex-row flex-wrap">
+          <span className="px-4 py-2">Learning</span>{" "}
+          <span className="text-zinc-950 bg-linear-to-r from-js-yellow to-js-yellow/1 px-4 py-2 lg:bg-linear-to-r lg:from-js-yellow/1 via-js-yellow to-js-yellow/">
+            JavaScript
+          </span>
+          <span className="bg-linear-to-r from-ts-blue to ts-blue/1 px-4 py-2">TypeScript</span>{" "}
+          <span className="text-zinc-950 bg-linear-to-r from-react-blue to-react-blue/1 px-4 py-2">React</span>
         </h1>
       </div>
       <div className="flex justify-end">
